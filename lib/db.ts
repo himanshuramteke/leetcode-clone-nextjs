ae2c9ce9666b313f1230e5d4e37c278da8521e7b
Himanshu Ramteke <himanshuramteke007@gmail.com>
2026-02-16 21:31:45 +0530

    added onBoardUser api

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
+const createPrismaClient = () => {
+  const connectionString = process.env.DATABASE_URL!;
+
+  if (!connectionString) {
+    throw new Error("DATABASE_URL environment variable is not set");
+  }
+  const pool = new Pool({ connectionString });
+  //@ts-expect-error
+  const adapter = new PrismaNeon(pool); // Type assertion to bypass the error
+
+  return new PrismaClient({
+    adapter,
   });
+};
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
---
+++ modules/auth/actions/index.ts
@@ -0,0 +1,59 @@
+"use server";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { User } from "@prisma/client";
+
+type OnBoardUserResponse =
+  | {
+      success: true;
+      user: User;
+      message: string;
+    }
+  | {
+      success: false;
+      error: string;
+    };
+
+
+export const onBoardUser = async (): Promise<OnBoardUserResponse> => {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      return { success: false, error: "No authenticated user found" };
+    }
+
+    const { id, firstName, lastName, imageUrl, emailAddresses } = user;
+
+    const newUser = await db.user.upsert({
+      where: {
+        clerkId: id,
+      },
+      update: {
+        firstName: firstName || null,
+        lastName: lastName || null,
+        imageUrl: imageUrl || null,
+        email: emailAddresses[0]?.emailAddress || "",
+      },
+      create: {
+        clerkId: id,
+        firstName: firstName || null,
+        lastName: lastName || null,
+        imageUrl: imageUrl || null,
+        email: emailAddresses[0]?.emailAddress || "",
+      },
+    });
+
+    return {
+      success: true,
+      user: newUser,
+      message: "User onBoarded successfully",
+    };
+  } catch (error: unknown) {
+    console.log("Error onboarding user", error);
+    return {
+      success: false,
+      error: "Failed to onboard user",
+    };
+  }
+};
